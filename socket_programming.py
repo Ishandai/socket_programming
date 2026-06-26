dd11af79a6c279712d6552d939ab601066691a42
ishan <ishan.077bei017@acem.edu.np>
2026-06-26 18:02:49 +0545

    added comments

--- socket_programming.py
+++ socket_programming.py
@@ -4,5 +4,5 @@ import socket
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 7777
 
-S=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+S=socket.socket(socket.AF_INET, socket.SOCK_STREAM)#Af_INET is the address family for IPv4. SOCK_STREAM means connection oriented TCP protocol
 S.connect((HOST, PORT))
