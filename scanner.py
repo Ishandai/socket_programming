a58e531a269302e707dc5ad3a6d9dbdf16de2b98
ishan <ishan.077bei017@acem.edu.np>
2026-06-26 18:15:24 +0545

    added scanner.py

---
+++ scanner.py
@@ -0,0 +1,36 @@
+import sys
+import socket
+form datetime import datetime
+
+#define our target host
+
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1]) #translate hostname to IPv4
+else:
+    print("Invalid amount of arguments.")
+    print("Syntax: python scanner.py <ip>")
+    sys.exit()
+
+#add a pretty banner
+print("-" * 50)
+print("Scanning Target: " + target)
+print("Time started: " + str(datetime.now()))
+print("-" * 50)
+
+try:
+    for port in range(1, 65535): #scan all ports
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1) #timeout for the connection
+        result = s.connect_ex((target, port)) #returns an error indicator
+        if result == 0:
+            print("Port {} is open".format(port))
+        s.close()
+except KeyboardInterrupt:
+    print("\nExiting program.")
+    sys.exit()
+except socket.gaierror:
+    print("Hostname could not be resolved.")
+    sys.exit()
+except socket.error:
+    print("Could not connect to server.")
+    sys.exit()
